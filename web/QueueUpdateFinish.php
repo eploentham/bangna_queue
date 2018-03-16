a6816553ccb3167e64a174c583f2044245480d0b
Ekapop <eploentham@gmail.com>
2018-03-16 18:27:55 +0700

    1 d

--- web/QueueUpdateFinish.php
+++ web/QueueUpdateFinish.php
@@ -2,38 +2,47 @@
 //include "connectdb.inc.php";
 
 $resultArray = array();
+$tmp = array();
 //$resultArray["area"] = array();
 $conn = mysqli_connect("localhost",$_POST['userdb'],$_POST['passworddb']);
 $objDB = mysqli_select_db($conn,"bangna_queue");
 mysqli_query($conn,"SET NAMES UTF8");
-$sql = "Insert Into t_queue(staff_id, queue_date, row_1, queue_active, status_queue, date_begin)"
-."Values('".$_POST['staff_id']."',now(),'".$_POST['row_1']."','1','1', now())";
+$sql = "Update t_queue Set status_queue = '2', date_finish = now() Where queue_id = '".$_POST['queue_id']."'";
 mysqli_query($conn,$sql);
-if($_POST['row_1']==="1"){
-	$sql = "Update b_staff Set queue_current = '".$_POST['row_1']."', queue_date = now() "
-		."Where staff_id = '".$_POST['staff_id']."'";
-	mysqli_query($conn,$sql);	
+
+$queueCurrent="";
+$sql = "Select * From t_queue Where staff_id = '".$_POST['staff_id']."' and status_queue = '1' Order By queue_id asc limit 1";
+if ($result=mysqli_query($conn,$sql)){
+	$intNumRows = mysqli_num_rows($result);
+	if($intNumRows>0){
+		while($row = mysqli_fetch_array($result)){
+			$queueCurrent = $row["row_1"];
+			$tmp["row_1"] = $row["row_1"];
+		}
+	}else{
+		$tmp["row_1"] = "0";
+	}
 }
+//$queueCurrent = intval($queueCurrent)+1;
+$sql = "Update b_staff Set queue_current ='".$queueCurrent."' Where staff_id = '".$_POST['staff_id']."'";
+mysqli_query($conn,$sql);
+
 
 $sql = "Select count(1) as cnt From t_queue Where staff_id = '".$_POST['staff_id']."' and status_queue = '1' ";
 if ($result=mysqli_query($conn,$sql)){
 	$intNumRows = mysqli_num_rows($result);
 	if($intNumRows>0){
 		while($row = mysqli_fetch_array($result)){
-			$tmp = array();
 			$tmp["onhand"] = $row["cnt"];
 		}
 	}else{
-		$tmp = array();
 			$tmp["onhand"] = "0";
 	}
 }else{
-	$tmp = array();
 	$tmp["onhand"] = "0";
 }
 
-//$tmp = array();
-$tmp["remark"] = $_POST['row_1'];
+//$tmp["remark"] = $_POST['row_1'];
 $tmp["success"] = "ok";
 array_push($resultArray,$tmp);
 mysqli_close($conn);
--- web/getDoctorQueueFirst.php
+++ web/getDoctorQueueFirst.php
@@ -1,17 +1,17 @@
 <?php
 //include "connectdb.inc.php";
 $resultArray = array();
-$tmp = array();
 $today = date("d.m.y");
 
 $conn = mysqli_connect("localhost",$_POST['userdb'],$_POST['passworddb']);
 $objDB = mysqli_select_db($conn,"bangna_queue");
 mysqli_query($conn,"SET NAMES UTF8");
-$sql = "Select * From t_queue Where staff_id = '".$_POST['staff_id']."' and status_queue = '1' Order By queue_id desc limit 1";
+$sql = "Select * From t_queue Where staff_id = '".$_POST['staff_id']."' and status_queue = '1' Order By queue_id asc limit 1";
 if ($result=mysqli_query($conn,$sql)){
 	$intNumRows = mysqli_num_rows($result);
 	if($intNumRows>0){
 		while($row = mysqli_fetch_array($result)){
+			$tmp = array();
 			$tmp["queue_id"] = $row["queue_id"];
 		    $tmp["staff_id"] = $row["staff_id"];
 		    $tmp["queue_date"] = $row["queue_date"];
@@ -25,15 +25,18 @@ if ($result=mysqli_query($conn,$sql)){
 		}
 		
 	}else{
+		$tmp = array();
 		$tmp["queue_id"] = "0";
 		$tmp["row_1"] = "0";
 		$tmp["remark"] = "";
 	}
 }else{
+	$tmp = array();
 	$tmp["queue_id"] = "0";
 	$tmp["row_1"] = "0";
 	$tmp["remark"] = $today;
 }
+
 $sql = "Select count(1) as cnt From t_queue Where staff_id = '".$_POST['staff_id']."' and status_queue = '1' ";
 if ($result=mysqli_query($conn,$sql)){
 	$intNumRows = mysqli_num_rows($result);
@@ -48,11 +51,8 @@ if ($result=mysqli_query($conn,$sql)){
 	$tmp["onhand"] = "0";
 }
 
-
-mysqli_query($conn,$sql);
-
-mysqli_close($conn);
 array_push($resultArray,$tmp);
+mysqli_close($conn);
 header('Content-Type: application/json');
 echo json_encode($resultArray);
 ?>
